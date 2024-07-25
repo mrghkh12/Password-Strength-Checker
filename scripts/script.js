5a910faab88a6947a4c8ca160ce6e5f25ec352d1
mohammad reza <mrghkh12@gmail.com>
2024-07-25 13:15:55 +0330

    feat(script.js): edit pass checker & create lower case function

--- scripts/script.js
+++ scripts/script.js
@@ -6,23 +6,36 @@ const messageBox = $.querySelector('#messageBox')
 
 passwordInput.addEventListener('keyup' , () =>{
     let value = passwordInput.value
+    let msgElem = messageBox.querySelector('.message')
+    let passCounter = messageBox.querySelector('.passLength')
+    passCounter.innerHTML = passwordInput.value.length
+    
+    isHaveLowerCase(value)
 
     if (value.length < 8) {
-        console.log("Your password must be at least 8 characters");
+        msgElem.innerHTML = "Your password must be at least 8 characters"
     }
-    if (value.length > 32) {
-        console.log("Your password must be at max 32 characters");
+    else if (isHaveLowerCase(value)) {
+        msgElem.innerHTML = "good"
     }
-    if (value.search(/[a-z]/) < 0) {
-        console.log("Your password must contain at least one lower case letter."); 
-    }
-    if (value.search(/[A-Z]/) < 0) {
-        console.log("Your password must contain at least one upper case letter."); 
-    }
-    if (value.search(/[0-9]/) < 0) {
-        console.log("Your password must contain at least one digit.");
-    }
-   if (value.search(/[!@#\$%\^&\*_]/) < 0) {
-        console.log("Your password must contain at least special char from -[ ! @ # $ % ^ & * _ ]"); 
-    }
+//     if (value.search(/[A-Z]/) < 0) {
+//         console.log("Your password must contain at least one upper case letter."); 
+//     }
+//     if (value.search(/[0-9]/) < 0) {
+//         console.log("Your password must contain at least one digit.");
+//     }
+//    if (value.search(/[!@#\$%\^&\*_]/) < 0) {
+//         console.log("Your password must contain at least special char from -[ ! @ # $ % ^ & * _ ]"); 
+//     }
 })
+
+function isHaveLowerCase(value){
+    let haveLower = messageBox.querySelector('.lower')
+    if (value.search(/[a-z]/) != -1) {
+        haveLower.style.color = 'green'
+        return true
+    }else{
+        haveLower.style.color = 'green'
+        return false
+    }
+}
