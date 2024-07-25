12d593c416e6e8221f10e8d9b5959d2050de07d3
mohammad reza <mrghkh12@gmail.com>
2024-07-25 13:24:28 +0330

    feat(script.js): edit pass checker & create upper case function

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,17 @@ passwordInput.addEventListener('keyup' , () =>{
     let msgElem = messageBox.querySelector('.message')
     let passCounter = messageBox.querySelector('.passLength')
     passCounter.innerHTML = passwordInput.value.length
-    
-    isHaveLowerCase(value)
 
-    if (value.length < 8) {
-        msgElem.innerHTML = "Your password must be at least 8 characters"
+    isHaveLowerCase(value)
+    isHaveUpperCase(value)
+
+     if (isHaveLowerCase(value)) {
+        msgElem.innerHTML = "Weak"
+        msgElem.style.color = 'red'
     }
-    else if (isHaveLowerCase(value)) {
-        msgElem.innerHTML = "good"
+     if (isHaveUpperCase(value)) {
+        msgElem.innerHTML = "Medium"
+        msgElem.style.color = 'yellow'
     }
 //     if (value.search(/[A-Z]/) < 0) {
 //         console.log("Your password must contain at least one upper case letter."); 
@@ -27,6 +30,10 @@ passwordInput.addEventListener('keyup' , () =>{
 //    if (value.search(/[!@#\$%\^&\*_]/) < 0) {
 //         console.log("Your password must contain at least special char from -[ ! @ # $ % ^ & * _ ]"); 
 //     }
+    if (value.length < 8) {
+        msgElem.innerHTML = "Your password must be at least 8 characters"
+        msgElem.style.color = '#fff'
+    }
 })
 
 function isHaveLowerCase(value){
@@ -35,7 +42,17 @@ function isHaveLowerCase(value){
         haveLower.style.color = 'green'
         return true
     }else{
-        haveLower.style.color = 'green'
+        haveLower.style.color = 'red'
+        return false
+    }
+}
+function isHaveUpperCase(value){
+    let haveUpper = messageBox.querySelector('.upper')
+    if (value.search(/[A-Z]/) != -1) {
+        haveUpper.style.color = 'green'
+        return true
+    }else{
+        haveUpper.style.color = 'red'
         return false
     }
 }
